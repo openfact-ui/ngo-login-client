1592aa7337b69bf26a574e9cd18e36f7f413282f
carlosthe19916@gmail.com <carlosthe19916@gmail.com>
2017-07-11 11:23:42 -0500

    add first template

---
+++ src/app/auth/authentication.service.ts
@@ -0,0 +1,194 @@
+import { Injectable, Inject } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+
+import { Observable, Subject } from 'rxjs';
+import { Broadcaster } from 'ngx-base';
+
+import { AUTH_API_URL } from '../shared/auth-api';
+import { SSO_API_URL } from '../shared/sso-api';
+import { REALM } from '../shared/realm-token';
+import { Token } from '../user/token';
+
+export interface ProcessTokenResponse {
+  (response: Response): Token;
+}
+
+@Injectable()
+export class AuthenticationService {
+
+  public microsoftToken: Observable<string>;
+  public googleToken: Observable<string>;
+  private refreshInterval: number;
+  private apiUrl: string;
+  private ssoUrl: string;
+  private realm: string;
+  private clearTimeoutId: any;
+  private refreshTokens: Subject<Token> = new Subject();
+  readonly microsoft = 'microsoft';
+  readonly google = 'google';
+
+  constructor(private broadcaster: Broadcaster,
+              @Inject(AUTH_API_URL) apiUrl: string,
+              @Inject(SSO_API_URL) ssoUrl: string,
+              @Inject(REALM) realm: string,
+              private http: Http) {
+    this.apiUrl = apiUrl;
+    this.ssoUrl = ssoUrl;
+    this.realm = realm;
+    this.microsoftToken = this.createFederatedToken(this.microsoft, (response: Response) => response.json() as Token);
+    this.googleToken = this.createFederatedToken(this.google, (response: Response) => this.queryAsToken(response.text()));
+  }
+
+  logIn(tokenParameter: string): boolean {
+    let tokenJson = decodeURIComponent(tokenParameter);
+    let token = this.processTokenResponse(JSON.parse(tokenJson));
+    this.setupRefreshTimer(token.expires_in);
+
+    // make sure old tokens are cleared out when we login again
+    localStorage.removeItem(this.microsoft + '_token');
+    localStorage.removeItem(this.google + '_token');
+
+    // kick off initial token refresh
+    this.refreshTokens.next(token);
+
+    this.onLogIn();
+    return true;
+  }
+
+  onLogIn() {
+    this.broadcaster.broadcast('loggedin', 1);
+  }
+
+  logout() {
+    this.clearSessionData();
+    this.broadcaster.broadcast('logout', 1);
+  }
+
+  isLoggedIn(): boolean {
+    let token = localStorage.getItem('auth_token');
+    if (token) {
+      if (!this.clearTimeoutId) {
+        // kick off initial token refresh
+        this.refreshTokens.next({ "access_token": token } as Token);
+        this.setupRefreshTimer(15);
+      }
+      return true;
+    }
+    return false;
+  }
+
+  getToken() {
+    if (this.isLoggedIn()) return localStorage.getItem('auth_token');
+  }
+
+  getGoogleToken(): Observable<string> {
+    if (localStorage.getItem(this.google + '_token')) {
+      return Observable.of(localStorage.getItem(this.google + '_token'));
+    }
+    return this.googleToken;
+  }
+
+  getMicrosoftToken(): Observable<string> {
+    if (localStorage.getItem(this.microsoft + '_token')) {
+      return Observable.of(localStorage.getItem(this.microsoft + '_token'));
+    }
+    return this.microsoftToken;
+  }
+
+  setupRefreshTimer(refreshInSeconds: number) {
+    if (!this.clearTimeoutId) {
+      // refresh should be required to be less than ten minutes measured in seconds
+      let tenMinutes = 60 * 10;
+      if (refreshInSeconds > tenMinutes) {
+        refreshInSeconds = tenMinutes;
+      }
+      let refreshInMs = Math.round(refreshInSeconds * .9) * 1000;
+      console.log('Refreshing token in: ' + refreshInMs + ' milliseconds.');
+      this.refreshInterval = refreshInMs;
+      if (process.env.ENV !== 'inmemory') {
+        // setTimeout() uses a 32 bit int to store the delay. So the max value allowed is 2147483647
+        // The bigger number will cause immediate refreshing
+        // but since we refresh in 10 minutes or in refreshInSeconds whatever is sooner we are good
+        this.clearTimeoutId = setTimeout(() => this.refreshToken(), refreshInMs);
+      }
+    }
+  }
+
+  refreshToken() {
+    if (this.isLoggedIn()) {
+      let headers = new Headers({ 'Content-Type': 'application/json' });
+      let refreshTokenUrl = this.apiUrl + 'login/refresh';
+      let refreshToken = localStorage.getItem('refresh_token');
+      let body = JSON.stringify({ 'refresh_token': refreshToken });
+      this.http.post(refreshTokenUrl, body, headers)
+        .map((response: Response) => {
+          let responseJson = response.json();
+          let token = this.processTokenResponse(responseJson.token);
+          this.clearTimeoutId = null;
+          this.setupRefreshTimer(token.expires_in);
+          return token;
+        })
+        .catch(response => {
+          // Additionally catch a 400 from keycloak
+          if (response.status === 400) {
+            this.broadcaster.broadcast('authenticationError', response);
+          }
+          return Observable.of({} as Token);
+        })
+        .subscribe(token => {
+          // Refresh any federated tokens that we have
+          this.refreshTokens.next(token);
+          console.log('token refreshed at:' + Date.now());
+        });
+    }
+  }
+
+  processTokenResponse(response: any): Token {
+    let token = response as Token;
+    localStorage.setItem('auth_token', token.access_token);
+    localStorage.setItem('refresh_token', token.refresh_token);
+    return token;
+  }
+
+  private createFederatedToken(broker: string, processToken: ProcessTokenResponse): Observable<string> {
+    let res = this.refreshTokens.switchMap(token => {
+      let headers = new Headers({ 'Content-Type': 'application/json' });
+      let tokenUrl = this.ssoUrl + `auth/realms/${this.realm}/broker/${broker}/token`;
+      headers.set('Authorization', `Bearer ${token.access_token}`);
+      let options = new RequestOptions({ headers: headers });
+      return this.http.get(tokenUrl, options)
+        .map(response => processToken(response))
+        .catch(response => {
+          if (response.status === 400) {
+            this.broadcaster.broadcast('noFederatedToken', res);
+          }
+          return Observable.of({} as Token);
+        })
+        .do(token => localStorage.setItem(broker + '_token', token.access_token))
+        .map(t => t.access_token);
+    }).publishReplay(1);
+    res.connect();
+    return res;
+  }
+
+  private queryAsToken(query: string): Token {
+    let vars = query.split('&');
+    let token = {} as any;
+    for (let i = 0; i < vars.length; i++) {
+      let pair = vars[i].split('=');
+      let key = decodeURIComponent(pair[0]);
+      let val = decodeURIComponent(pair[1]);
+      token[key] = val;
+    }
+    return token as Token;
+  }
+
+  private clearSessionData(): void {
+    localStorage.removeItem('auth_token');
+    localStorage.removeItem('refresh_token');
+    localStorage.removeItem(this.microsoft + '_token');
+    localStorage.removeItem(this.google + '_token');
+    clearTimeout(this.clearTimeoutId);
+    this.refreshInterval = null;
+  }
+}
---
+++ src/app/user/user.service.ts
@@ -0,0 +1,183 @@
+import { Injectable, Inject } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import {
+  Observable,
+  ConnectableObservable,
+  ReplaySubject,
+  Subject
+} from 'rxjs';
+
+import { cloneDeep } from 'lodash';
+import { Broadcaster, Logger } from 'ngx-base';
+
+import { AUTH_API_URL } from '../shared/auth-api';
+import { User } from './user';
+
+/**
+ *  Provides user and user list methods to retrieve current or user list details
+ *
+ *  The UserService should be injected at the root of the application to ensure it is a singleton
+ *  getUser and getAllUsers return observables that can be subscribed to for information
+ */
+@Injectable()
+export class UserService {
+
+  /**
+   * The currently logged in user
+   */
+  public loggedInUser: ConnectableObservable<User>;
+
+  /**
+   * @deprecated since v0.4.4. Use {@link #loggedInUser} instead.
+   */
+  private userData: User = {} as User;
+
+  /**
+   * @deprecated since v0.4.4. No replacement method is provided.
+   */
+  private allUserData: User[] = [];
+
+  private headers = new Headers({ 'Content-Type': 'application/json' });
+  private userUrl: string;  // URL to web api
+  private usersUrl: string;  // URL to web api
+  private searchUrl: string;
+
+  constructor(private http: Http,
+              private logger: Logger,
+              broadcaster: Broadcaster,
+              @Inject(AUTH_API_URL) apiUrl: string) {
+    this.userUrl = apiUrl + 'user';
+    this.usersUrl = apiUrl + 'users';
+    this.searchUrl = apiUrl + 'search';
+    this.loggedInUser = Observable.merge(
+      broadcaster.on('loggedin')
+        .map(val => 'loggedIn'),
+      broadcaster.on('logout')
+        .map(val => 'loggedOut'),
+      broadcaster.on('authenticationError')
+        .map(val => 'authenticationError')
+    )
+      .switchMap(val => {
+        // If it's a login event, then we need to retreive the user's details
+        if (val === 'loggedIn') {
+          return this.http
+            .get(this.userUrl, { headers: this.headers })
+            .map(response => cloneDeep(response.json().data as User));
+        } else {
+          // Otherwise, we clear the user
+          return Observable.of({} as User);
+        }
+      })
+      // TODO remove this
+      .do(user => this.userData = user)
+      // In order to ensure any future subscribers get the currently user
+      // we use a replay subject of size 1
+      .multicast(() => new ReplaySubject(1));
+    this.loggedInUser.connect();
+  }
+
+  /**
+   * Get the User object for a given user id, or null if no user is found
+   * @param userId the userId to search for
+   */
+  getUserByUserId(userId: string): Observable<User> {
+    return this.http
+      .get(`${this.usersUrl}/${userId}`, { headers: this.headers })
+      .map(response => {
+        return response.json().data as User;
+      });
+  }
+
+  /**
+   * Get the User object for a given username, or null if no user is found
+   * @param username the username to search for
+   */
+  getUserByUsername(username: string): Observable<User> {
+    return this.filterUsersByUsername(username).map(val => {
+      for (let u of val) {
+        if (username === u.attributes.username) {
+          return u;
+        }
+      }
+      return null;
+    });
+  }
+
+  /**
+   * Get users by a search string
+   */
+  getUsersBySearchString(search: string): Observable<User[]> {
+    if (search && search !== "") {
+      return this.http
+        .get(this.searchUrl + '/users?q=' + search, { headers: this.headers })
+        .map(response => {
+          return response.json().data as User[];
+        });
+    }
+    return Observable.of([] as User[]);
+  }
+
+  /**
+   * @deprecated since v0.4.4. Use {@link #loggedInUser} instead.
+   */
+  getSavedLoggedInUser(): User {
+    return this.userData;
+  }
+
+  /**
+   * @deprecated since v0.4.4. No replacement is provided.
+   */
+  getLocallySavedUsers(): User[] {
+    return this.allUserData;
+  }
+
+  /**
+   * @deprecated since v0.4.4. Use {@link #loggedInUser} instead.
+   * Get currently logged in user
+   *
+   * @returns Observable<User>
+   */
+  getUser(): Observable<User> {
+    return this.loggedInUser;
+  }
+
+  /**
+   * @deprecated since v0.4.4. No replacement is provided.
+   * Get all users
+   *
+   * @returns Observable<User[]>
+   */
+  getAllUsers(): Observable<User[]> {
+    return this.http
+      .get(this.usersUrl, { headers: this.headers })
+      .map(response => {
+        return response.json().data as User[];
+      })
+      // TODO remove this
+      .do(val => this.allUserData = val);
+  }
+
+  /**
+   *
+   * Filter users by username
+   *
+   * @returns Observable<User[]>
+   */
+
+  filterUsersByUsername(username: string): Observable<User[]> {
+    return this.http
+      .get(`${this.usersUrl}?filter[username]=${username}`, { headers: this.headers })
+      .map(response => {
+        return response.json().data as User[];
+      })
+  }
+
+  /**
+   * @deprecated since v0.4.4. No replacement is provided.
+   *
+   */
+  resetUser(): void {
+    this.userData = {} as User;
+  }
+}
