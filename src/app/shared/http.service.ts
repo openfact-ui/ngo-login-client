8d5a6a600f12eab107e5798c9dbb2615096c1579
Carlos Feria <carlosthe19916@gmail.com>
2017-07-17 19:03:03 -0500

    change module

--- src/app/shared/http.service.ts
+++ src/app/shared/http.service.ts
@@ -1,46 +1,36 @@
-import { Injectable, Inject, forwardRef } from '@angular/core';
+import {Injectable, Inject, forwardRef} from '@angular/core';
 import {
-  Headers,
-  Http,
   Response,
+  RequestOptionsArgs,
   RequestOptions,
   Request,
-  RequestOptionsArgs,
-  XHRBackend
+  ConnectionBackend
 } from '@angular/http';
 
-import { Observable } from 'rxjs/Observable';
+import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/of';
 
-import { Broadcaster } from 'ngo-base';
+import {Broadcaster} from 'ngo-base';
+
+import {Keycloak, KeycloakHttp, KeycloakAuthorization} from '@ebondu/angular2-keycloak';
 
 @Injectable()
-export class HttpService extends Http {
+export class HttpService extends KeycloakHttp {
+
   private broadcaster: Broadcaster;
 
-  constructor(backend: XHRBackend,
-              options: RequestOptions,
+  constructor(backend: ConnectionBackend,
+              defaultOptions: RequestOptions,
+              keycloak: Keycloak,
+              keycloakAuth: KeycloakAuthorization,
               @Inject(forwardRef(() => Broadcaster)) broadcaster: Broadcaster) {
-    super(backend, options);
+    super(backend, defaultOptions, keycloak, keycloakAuth);
     this.broadcaster = broadcaster;
   }
 
   request(url: string | Request, options?: RequestOptionsArgs): Observable<Response> {
-    let token = localStorage.getItem('auth_token');
-    if (token !== null) {
-      if (typeof url === 'string') { // meaning we have to add the token to the options, not in url
-        if (!options) {
-          // let's make option object
-          options = { headers: new Headers() };
-        }
-        options.headers.set('Authorization', `Bearer ${token}`);
-      } else {
-        // we have to add the token to the url object
-        url.headers.set('Authorization', `Bearer ${token}`);
-      }
-    }
     return super.request(url, options).catch(this.catchRequestError());
   }
 
