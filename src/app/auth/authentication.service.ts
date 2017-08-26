eb5d15418d5fb1722a0818f9eac9c799f9f9f3ed
Carlos Feria <carlosthe19916@gmail.com>
2017-08-26 09:28:20 -0500

    fix(package): force update

--- src/app/auth/authentication.service.ts
+++ src/app/auth/authentication.service.ts
@@ -27,6 +27,7 @@ export class AuthenticationService {
   // Keycloak utils
   public isAuthenticated: boolean;
 
+  // Keycloak location
   private apiUrl: string;
   private ssoUrl: string;
   private realm: string;
