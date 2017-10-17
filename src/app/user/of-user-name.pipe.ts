0d06994006b3152bf4ac198cf81d72e9934449dd
Carlos Feria <carlosthe19916@gmail.com>
2017-10-17 12:56:44 -0500

    fix(package): update ngo-base to 0.7.0

--- src/app/user/of-user-name.pipe.ts
+++ src/app/user/of-user-name.pipe.ts
@@ -4,7 +4,7 @@ import { User } from './user';
 
 // tslint:disable-next-line:use-pipe-transform-interface
 @Pipe({ name: 'ofUserName', pure: true })
-export class ofUserName implements PipeTransform {
+export class OfUserName implements PipeTransform {
   // TODO: find out if "null = null" is needed or can just be "null"
   transform(userObj: User | null = null, notFound: string = 'User not found'): string {
     if (typeof (userObj) === 'undefined' || userObj === null) {
